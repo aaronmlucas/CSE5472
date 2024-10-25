211fd0035e1d55870d1ed4cb18d6f97e9a096992
Sebastian <bell.1712@buckeyemail.osu.edu>
2024-10-25 10:24:43 -0400

    Added basic host statistic recording

--- KDD.py
+++ KDD.py
@@ -2,20 +2,21 @@ import sys
 import os
 import gzip
 import pyshark
+from collections import defaultdict
 
-cat = ["duration", "protocol_type", "service", "src_bytes", "dst_bytes","flag", "land", "wrong_fragment", "urgent",
-        "hot", "num_failed_logins", "logged_in", "num_compromised", "root_shell", "su_attempted", "num_root", "num_file_creations"
-        "num_shells", "num_access_files", "num_outbound_cmds", "is_host_login", "is_guest_login", "count", "srv_count", "serror_rate",
-        "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate", "diff_srv_rate", "srv_diff_host_rate", "dst_host_count",
-        "dst_host_srv_count", "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate",
-        "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate", "dst_host_same_src_port_rate",
-        "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate", "dst_host_rerror_rate", "dst_host_srv_rerror_rate"]
+host_stat = defaultdict(lambda: {
+    'count': 0,
+    'serror_count': 0,
+    'rerror_count': 0,
+    'same_srv_count': 0
+})
 
 def parse_log(log_fp):
     capture = pyshark.FileCapture(log_fp)
     data = []
     for packet in capture:
         data.append(parse_packet(packet))
+    calculate_host_stat()
     return data
 
 
@@ -25,35 +26,33 @@ def parse_packet(packet):
         packet_info['timestamp'] = packet.sniff_time
         packet_info['protocol'] = packet.transport_layer
         packet_info['tcp_flags'] = packet.tcp.flags
+        packet_info['src_port'] = packet.tcp.srcport
+        packet_info['dest_port'] = packet.tcp.dstport   
+        if packet.tcp.dstport == '80': # Add option for more ports
+            host_stat[packet.ip.src]['same_srv_count'] += 1
+        if 'SYN' and 'ACK' in packet.tcp.flags: 
+            host_stat[packet.ip.src]['serror_count'] += 1
+        elif 'RST' in packet.tcp.flags:
+            host_stat[packet.ip.src]['rerror_count'] += 1
     if 'IP' in packet:
         packet_info['src'] = packet.ip.src
+        host_stat[packet.ip.src]['count'] += 1
         packet_info['dest'] = packet.ip.src
     if 'HTTP' in packet and packet.http.request_method == 'GET':
         packet_info['num_file_creations'] = 1
     return packet_info
 
-
-
-    # log = list()
-    # eid = 0
-    # with gzip.open(log_fp, "rt") as ifile:
-    #     for line in ifile:
-    #         event = parse_data(line);
-    #         if event is None:
-    #             continue
-    #         log.append((eid, event))
-    #         eid += 1
-    #return log
-
-def parse_data(line):
-    pairs = line.strip().split(",")
-    return dict(zip(cat, pairs))
-    
+def calculate_host_stat():
+    for host, stat in host_stat.items():
+        stat["serror_count"] = stat["serror_count"] / stat['count'] if stat['count'] > 0 else 0
+        stat["rerror_count"] = stat["rerror_count"] / stat['count'] if stat['count'] > 0 else 0
+        stat["same_srv_count"] = stat["same_srv_count"] / stat['count'] if stat['count'] > 0 else 0
 
 def main():
     if len(sys.argv) != 2:
         print("Please pass in the data set.")
     print(parse_log(sys.argv[1]))
+    print(host_stat)
 
 if __name__ == "__main__":
     main()
